94eef6574a969dcbb94390c5ffe2b13218cb5e4f
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-08-05 22:36:31 -0300

    Revisão: Introdução de estrutura de repetição - while

--- aula-02-revisao/revisao.java
+++ aula-02-revisao/revisao.java
@@ -10,9 +10,18 @@ void main() {
 
     for (int indice = 0; indice < quantidadeNotas; indice++) {
         int numeroProva = indice + 1;
-        System.out.println("Digite a nota da prova " + 
-            numeroProva + ":" );
-        double nota = scanner.nextDouble();
+        double nota = 0.0;
+        boolean notaValida = false;
+        do {
+            System.out.println("Digite a nota da prova " + 
+                numeroProva + " (0 a 10):" );
+            nota = scanner.nextDouble();
+            notaValida = (nota >= 0) && (nota <= 10);
+            if (!notaValida) {
+                System.out.println("Você deve informar um valor" +
+                    " entre 0 e 10");
+            }
+        } while (!notaValida);
         somaNotas += nota;
     }
 
