86a2197565bba1cbb03afb16868e10703310cdfc
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-08-12 20:51:26 -0300

    Revisão: Introdução de Modularização

--- aula-02-revisao/revisao.java
+++ aula-02-revisao/revisao.java
@@ -10,34 +10,61 @@ void main() {
 
     for (int indice = 0; indice < quantidadeNotas; indice++) {
         int numeroProva = indice + 1;
-        double nota = 0.0;
-        boolean notaValida = false;
-        do {
-            System.out.println("Digite a nota da prova " + 
-                numeroProva + " (0 a 10):" );
-            nota = scanner.nextDouble();
-            notaValida = (nota >= 0) && (nota <= 10);
-            if (!notaValida) {
-                System.out.println("Você deve informar um valor" +
-                    " entre 0 e 10");
-            }
-        } while (!notaValida);
+        String mensagem = "Digite a nota da prova " + 
+                numeroProva + " (0 a 10):";
+        double nota = lerDadoValido(
+            scanner, mensagem, 0, 10);
         somaNotas += nota;
     }
 
+    String mensagem = "Digite a frequência em % " +
+            "(0 a 100)";
+    double frequencia = lerDadoValido(
+        scanner, mensagem, 0, 100);
+
     // PROCESSAMENTO
     double media = somaNotas / quantidadeNotas;
-    String situacao;
-
-    if (media >= 7.0) {
-        situacao = "Aprovação direta!";
-    } else if (media >= 4.0) {
-        situacao = "Em exame";
-    } else {
-        situacao = "Reprovação direta";
-    }
+    String situacao = verificarSituacao(
+        media, frequencia);
 
     // SAÍDA
     System.out.println("Média: " + media);
     System.out.println("Situação: " + situacao);
+}
+
+double lerDadoValido(
+    Scanner scanner,
+    String mensagem,
+    double min,
+    double max
+) {
+    double valor = 0.0;
+    boolean valorValido = false;
+    do {
+        System.out.println(mensagem);
+        valor = scanner.nextDouble();
+        valorValido = (valor >= min) && 
+            (valor <= max);
+        if (!valorValido) {
+            System.out.println(
+                "Você deve informar um valor" +
+                " entre " + min + " e " + max);
+        }
+    } while (!valorValido);
+    return valor;
+}
+
+String verificarSituacao(
+    double media, 
+    double frequencia
+) {
+    if (frequencia < 75.0) {
+        return "Reprovação por frequência";
+    } else if (media >= 7.0) {
+        return "Aprovação direta!";
+    } else if (media >= 4.0) {
+        return "Em exame";
+    } else {
+        return "Reprovação por nota";
+    }
 }
