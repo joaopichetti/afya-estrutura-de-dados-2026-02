f14bb7159705e29769508ff8e198d13604f61b0a
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-08-19 20:59:47 -0300

    Revisão: Exemplo inicial de utilização de arrays e matrizes (incompleto)

--- aula-02/revisao/revisao.java
+++ aula-02/revisao/revisao.java
@@ -6,30 +6,28 @@ void main() {
     // ENTRADA
     System.out.println("Digite a quantidade de notas:");
     int quantidadeNotas = scanner.nextInt();
-    double somaNotas = 0.0;
+    final int MAX_ALUNOS = 5;
+    double[] frequencias = new double[MAX_ALUNOS];
+    double[][] notas = new double[MAX_ALUNOS][quantidadeNotas];
+    boolean[] cadastrados = new boolean[MAX_ALUNOS];
 
-    for (int indice = 0; indice < quantidadeNotas; indice++) {
-        int numeroProva = indice + 1;
-        String mensagem = "Digite a nota da prova " + 
-                numeroProva + " (0 a 10):";
-        double nota = lerDadoValido(
-            scanner, mensagem, 0, 10);
-        somaNotas += nota;
-    }
-
-    String mensagem = "Digite a frequência em % " +
-            "(0 a 100)";
-    double frequencia = lerDadoValido(
-        scanner, mensagem, 0, 100);
-
-    // PROCESSAMENTO
-    double media = somaNotas / quantidadeNotas;
-    String situacao = verificarSituacao(
-        media, frequencia);
-
-    // SAÍDA
-    System.out.println("Média: " + media);
-    System.out.println("Situação: " + situacao);
+    int opcao;
+    do {
+        System.out.println("\n=== SISTEMA DE NOTAS E FREQUÊNCIA ===");
+        System.out.println("1 - Cadastrar notas e frequência de um aluno");
+        System.out.println("2 - Imprimir situação de um aluno específico");
+        System.out.println("3 - Imprimir situação de todos os alunos");
+        System.out.println("4 - Sair");
+        String mensagem = "Escolha uma opção:";
+        opcao = (int) lerDadoValido(scanner, mensagem, 1, 4);
+        switch (opcao) {
+            case 1 -> ...;
+            case 2 -> ...;
+            case 3 -> ...;
+            case 4 -> System.out.println("\nSaindo do sistema. Até logo!");
+            default -> System.out.println("Opção inválida...");
+        }
+    } while (opcao != 4);
 }
 
 double lerDadoValido(
@@ -67,4 +65,37 @@ String verificarSituacao(
     } else {
         return "Reprovação por nota";
     }
+}
+
+int lerId(Scanner scanner, int maxAlunos) {
+    int maxId = maxAlunos - 1;
+    String mensagem = "Digite o ID do aluno (0 a " + maxId + "): ";
+    int id = (int) lerDadoValido(scanner, mensagem, 0, maxId);
+    return id;
+}
+
+void cadastrarAluno(
+    Scanner scanner,
+    double[] frequencias,
+    double[][] notas,
+    boolean[] cadastrados,
+    int quantidadeNotas,
+    int maxAlunos
+) {
+    int id = lerId(scanner, maxAlunos);
+
+    for (int indice = 0; indice < quantidadeNotas; indice++) {
+        int numeroProva = indice + 1;
+        String mensagem = "Digite a nota "+ numeroProva + ": ";
+        double nota = lerDadoValido(scanner, mensagem, 0, 10);
+        notas[id][indice] = nota;
+    }
+
+    String mensagem = "Difgite a frequência em % (0 a 100)";
+    double frequencia = lerDadoValido(scanner, mensagem, 0, 100);
+    frequencias[id] = frequencia;
+
+    cadastrados[id] = true;
+
+    System.out.println("Aluno ID " + id + " cadastrado com sucesso!");
 }
